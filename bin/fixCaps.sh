b9ff2c45d39a07f285db04dc764d1a055806e103
Mike Battles <mike.battles@gmail.com>
2016-12-21 11:17:15 -0500

    adding fixCaps to bin dir

--- bin/fixCaps.sh
+++ bin/fixCaps.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-#This script will remap Caps -> ESC
+#xmodmap -e "keycode 9 = Caps_Lock NoSymbol Caps_Lock"   #this will make Esc to act as Caps Lock
 xmodmap -e "keycode 66 = Escape NoSymbol Escape"
+xmodmap -e "keycode 9 = Escape NoSymbol Escape"
+xmodmap -e "clear lock"
